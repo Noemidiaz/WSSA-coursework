2737845d0b0f9a4b159989dd0348a2fc25dabadc
Noemi Diaz <noemi.diazglez@gmail.com>
2025-04-21 16:25:09 +0100

    creating file config.py

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,22 @@
+# assignment04-github.py
+# Description:
+# By Noemi Diaz
+
+
+# Step 1: Install PyGithub in Python using terminal
+# pip install PyGithub
+
+
+# Step 2: Create a Github Token
+
+# Step 3: Python code to access test.txt
+
+from github import Github
+from config import config as cfg
+
+apikey = cfg["githubkey"]
+
+# Using own key
+g = Github(apikey)
+for repo in g.get_user().get_repos():
+ print(repo.name)
