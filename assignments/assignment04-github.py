da096c64fb3b3698a94181e6400ae505f5676931
Noemi Diaz <noemi.diazglez@gmail.com>
2025-04-21 20:46:02 +0100

    moving config.py to subfolder

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -15,10 +15,9 @@ from github import Github
 from config import config as cfg
 
 apikey = cfg["githubkey"]
-
+ 
 # Using own key
 g = Github(apikey)
 for repo in g.get_user().get_repos():
  print(repo.name)
 
- #
